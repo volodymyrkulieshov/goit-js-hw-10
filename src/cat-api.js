0115dbfc5788e8e6a1eae30e846113d57ccbdc7e
Volodymyr <vovkul@gmail.com>
2023-10-25 12:00:34 +0200

    version 1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,31 +1,49 @@
-// import axios from 'axios';
-
-const BASE_URL = 'https://api.thecatapi.com/v1';
+const URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_mw1wBFgWwKML3FhpuSEsISiVgwRZVBIhpdCjmemzslSsGbt2r1BAtVkT7YtRxOLe';
 
-// axios.defaults.headers.common['x-api-key'] = `${API_KEY}`;
-
 export function fetchBreeds() {
-  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
-    // console.log(response);
+  return fetch(`${URL}/breeds?api_key=${API_KEY}`).then(response => {
     if (!response.ok) {
-      throw new Error(response.statusText);
+      throw new Error(response.status);
     }
     return response.json();
   });
 }
-fetchBreeds();
 
 export function fetchCatByBreed(breedId) {
   return fetch(
-    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+    `${URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
   ).then(response => {
-    console.log(response);
     if (!response.ok) {
-      throw new Error(response.statusText);
+      throw new Error(response.status);
     }
     return response.json();
   });
 }
-fetchCatByBreed();
+
+// ----------------USE AXIOS--------------------
+
+// export function fetchBreeds() {
+//   return axios.get(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
+//     console.log(response);
+//     if (response.status !== 200) {
+//       throw new Error(response.status);
+//     }
+//     return response;
+//   });
+// }
+// fetchBreeds();
+
+// export function fetchCatByBreed(breedId) {
+//   return axios
+//     .get(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
+//     .then(response => {
+//       console.log(response);
+//       if (response.status !== 200) {
+//         throw new Error(response.status);
+//       }
+//       return response;
+//     });
+// }
+// fetchCatByBreed();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const elements = {
+  select: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+};
+
+elements.catInfo.classList.add('is-hidden');
+
+elements.select.addEventListener('change', createMarkup);
+
+updateSelect();
+
+function updateSelect(data) {
+  fetchBreeds(data)
+    .then(data => {
+      elements.loader.classList.replace('loader', 'is-hidden');
+
+      let markSelect = data.map(({ name, id }) => {
+        return `<option value ='${id}'>${name}</option>`;
+      });
+      elements.select.insertAdjacentHTML('beforeend', markSelect);
+      new SlimSelect({
+        select,
+      });
+    })
+    .catch(onFetchError);
+}
+
+function createMarkup(event) {
+  elements.loader.classList.replace('is-hidden', 'loader');
+  elements.select.classList.add('is-hidden');
+  elements.catInfo.classList.add('is-hidden');
+
+  const breedId = event.currentTarget.value;
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      elements.loader.classList.replace('loader', 'is-hidden');
+      elements.select.classList.remove('is-hidden');
+      const { url, breeds } = data[0];
+
+      elements.catInfo.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/>
+            <div class="box">
+            <h2>${breeds[0].name}</h2>
+            <p>${breeds[0].description}</p>
+            <p><strong>Temperament:</strong> ${breeds[0].temperament}</p>
+            </div>`;
+      elements.catInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+}
+
+function onFetchError() {
+  elements.select.classList.remove('is-hidden');
+  elements.loader.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!'
+  );
+}
